4fad4cfb31178690ff13a23659c272f8a3e65733
Johannes Seebauer <johannes.seebauer7@gmail.com>
2016-09-22 20:52:55 +0200

    First commit

---
+++ Session3_HW/HW_3.R
@@ -0,0 +1,4 @@
+print("Sorry this is all I got...I solved my weekly time management 
+problem subject to massive time constraints. It turns 
+out the optimum gave me zero for the session 3 HW...but hey,
+at least I sumbitted it ;) ")
