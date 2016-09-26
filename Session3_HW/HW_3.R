7b4d8aaf047631db6d305a4b5b607767e763261c
Tyler Abbot <tyler.abbot@sciencespo.fr>
2016-09-26 12:48:33 +0200

    Tylers comments on weeks 1-3.

--- Session3_HW/HW_3.R
+++ Session3_HW/HW_3.R
@@ -1,4 +1,9 @@
-print("Sorry this is all I got...I solved my weekly time management 
-problem subject to massive time constraints. It turns 
+####### Comment
+# Ha! Thanks for the note, I understand things are busy and knew that not taking
+# a grade would mean this course would fall to the bottom of your preference
+# ordering, so no worries!
+
+print("Sorry this is all I got...I solved my weekly time management
+problem subject to massive time constraints. It turns
 out the optimum gave me zero for the session 3 HW...but hey,
 at least I sumbitted it ;) ")
