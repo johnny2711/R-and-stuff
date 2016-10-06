37828ac2d088d9872541ec438c3b6bbc4844ca90
Johannes Seebauer <johannes.seebauer7@gmail.com>
2016-10-06 23:16:15 +0200

    First commit

---
+++ Session5_HW/HW_5.R
@@ -0,0 +1,5 @@
+#Hi Tyler, this & next week my priority is to survive the midterms,
+#so I really haven't found the time to do the HW. However, thereafter 
+#the programming course has my undivided attention!!  
+#P.S: I appreciate that you take the time to add comments to each of the 
+#homeworks, this is very helpful in avoiding future mistakes!
